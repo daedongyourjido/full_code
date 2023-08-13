6d25ce0dbfa590b2420f9b8208448413b2f74e4c
sjhong98 <sjhong98@icloud.com>
2023-08-13 18:14:30 +0900

    feat: 오류 수정

--- src/map.js
+++ src/map.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {Link, useLocation, useNavigate} from 'react-router-dom';
 
 import geo from './1.json';
